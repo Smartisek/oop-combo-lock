e342154aedb94ba866a82c4df5718d10ccc6ad06
dermo <dermot.logue@dkit.ie>
2023-09-29 13:52:19 +0100

    added enum declaration

---
+++ src/main/java/oop/ComboLockState.java
@@ -0,0 +1,8 @@
+package oop;
+
+// This is an Enumeration (enum)
+// It should be used to make source code more readable
+// Instances of this enum can be assigned only the four values listed below
+
+public enum ComboLockState { NONE_CORRECT, FIRST_CORRECT, SECOND_CORRECT, ALL_CORRECT };
+
--- src/main/java/oop/LockStateEnum.java
+++
@@ -1,8 +0,0 @@
-package oop;
-
-// Enumeration class
-
-//TODO - consider enumeration to represent lock state
-// enum will make code more readable and less error prone
-
-
